eb2010b14522d068b8878512826566436b813d5e
jmutua <johnstone.m10@gmail.com>
2024-11-25 16:53:06 +0300

    replicated div

--- resources/js/Pages/Contacts.vue
+++ resources/js/Pages/Contacts.vue
@@ -24,58 +24,146 @@
                 <div
                     class="rounded border border-white text-white  opacity-50 hover:opacity-100 hover:cursor-pointer max-w-96 hover:border-orange-300">
                     <p class="p-2.5 text-justify">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Neque molestiae corporis maiores ullam
-                        iste praesentium officia vel. Sed quaerat sunt iste magnam architecto, suscipit velit libero
-                        deserunt natus maxime maiores.</p></div>
+                        iste praesentium officia vel. 
+                        <br> <br>
+                        <div class="pi pi-phone" style="font-size: 15px; vertical-align:middle;"></div>  Telephone: +255 123-456-780 
+                        <br>
+                        <div class="pi pi-inbox" style="font-size: 15px; vertical-align:middle;"></div>  Email Addr: test@example.com
+                        <br>
+                        <div class="pi pi-link" style="font-size: 15px; vertical-align:middle;"></div>   Website:   <a href="https://www.google.com" @click="showAlert" target="_blank">www.students-school.aca</a> 
+                        <br>
+                        <div class="pi pi-twitter" style="font-size: 15px; vertical-align:middle;"></div>  Formerly twitter | <div class="pi pi-facebook" style="font-size: 15px; vertical-align:middle;"></div> Facebook
+                    </p></div>
                 <div
                     class="rounded border border-white text-white  opacity-50 hover:opacity-100 hover:cursor-pointer max-w-96 hover:border-orange-300">
                     <p class="p-2.5 text-justify">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Neque molestiae corporis maiores ullam
-                        iste praesentium officia vel. Sed quaerat sunt iste magnam architecto, suscipit velit libero
-                        deserunt natus maxime maiores.</p></div>
+                        iste praesentium officia vel. 
+                        <br> <br>
+                        <div class="pi pi-phone" style="font-size: 15px; vertical-align:middle;"></div>  Telephone: +255 123-456-780 
+                        <br>
+                        <div class="pi pi-inbox" style="font-size: 15px; vertical-align:middle;"></div>  Email Addr: test@example.com
+                        <br>
+                        <div class="pi pi-link" style="font-size: 15px; vertical-align:middle;"></div>   Website:   <a href="https://www.google.com" @click="showAlert" target="_blank">www.students-school.aca</a> 
+                        <br>
+                        <div class="pi pi-twitter" style="font-size: 15px; vertical-align:middle;"></div>  Formerly twitter | <div class="pi pi-facebook" style="font-size: 15px; vertical-align:middle;"></div> Facebook
+                    </p></div>
                 <div
                     class="rounded border border-white text-white  opacity-50 hover:opacity-100 hover:cursor-pointer max-w-96 hover:border-orange-300">
                     <p class="p-2.5 text-justify">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Neque molestiae corporis maiores ullam
-                        iste praesentium officia vel. Sed quaerat sunt iste magnam architecto, suscipit velit libero
-                        deserunt natus maxime maiores.</p></div>
+                        iste praesentium officia vel. 
+                        <br> <br>
+                        <div class="pi pi-phone" style="font-size: 15px; vertical-align:middle;"></div>  Telephone: +255 123-456-780 
+                        <br>
+                        <div class="pi pi-inbox" style="font-size: 15px; vertical-align:middle;"></div>  Email Addr: test@example.com
+                        <br>
+                        <div class="pi pi-link" style="font-size: 15px; vertical-align:middle;"></div>   Website:   <a href="https://www.google.com" @click="showAlert" target="_blank">www.students-school.aca</a> 
+                        <br>
+                        <div class="pi pi-twitter" style="font-size: 15px; vertical-align:middle;"></div>  Formerly twitter | <div class="pi pi-facebook" style="font-size: 15px; vertical-align:middle;"></div> Facebook
+                    </p></div>
                 <div
                     class="rounded border border-white text-white  opacity-50 hover:opacity-100 hover:cursor-pointer max-w-96 hover:border-orange-300">
                     <p class="p-2.5 text-justify">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Neque molestiae corporis maiores ullam
-                        iste praesentium officia vel. Sed quaerat sunt iste magnam architecto, suscipit velit libero
-                        deserunt natus maxime maiores.</p></div>
+                        iste praesentium officia vel. 
+                        <br> <br>
+                        <div class="pi pi-phone" style="font-size: 15px; vertical-align:middle;"></div>  Telephone: +255 123-456-780 
+                        <br>
+                        <div class="pi pi-inbox" style="font-size: 15px; vertical-align:middle;"></div>  Email Addr: test@example.com
+                        <br>
+                        <div class="pi pi-link" style="font-size: 15px; vertical-align:middle;"></div>   Website:   <a href="https://www.google.com" @click="showAlert" target="_blank">www.students-school.aca</a> 
+                        <br>
+                        <div class="pi pi-twitter" style="font-size: 15px; vertical-align:middle;"></div>  Formerly twitter | <div class="pi pi-facebook" style="font-size: 15px; vertical-align:middle;"></div> Facebook
+                    </p></div>
                 <div
                     class="rounded border border-white text-white  opacity-50 hover:opacity-100 hover:cursor-pointer max-w-96 hover:border-orange-300">
                     <p class="p-2.5 text-justify">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Neque molestiae corporis maiores ullam
-                        iste praesentium officia vel. Sed quaerat sunt iste magnam architecto, suscipit velit libero
-                        deserunt natus maxime maiores.</p></div>
+                        iste praesentium officia vel. 
+                        <br> <br>
+                        <div class="pi pi-phone" style="font-size: 15px; vertical-align:middle;"></div>  Telephone: +255 123-456-780 
+                        <br>
+                        <div class="pi pi-inbox" style="font-size: 15px; vertical-align:middle;"></div>  Email Addr: test@example.com
+                        <br>
+                        <div class="pi pi-link" style="font-size: 15px; vertical-align:middle;"></div>   Website:   <a href="https://www.google.com" @click="showAlert" target="_blank">www.students-school.aca</a> 
+                        <br>
+                        <div class="pi pi-twitter" style="font-size: 15px; vertical-align:middle;"></div>  Formerly twitter | <div class="pi pi-facebook" style="font-size: 15px; vertical-align:middle;"></div> Facebook
+                    </p></div>
                 <div
                     class="rounded border border-white text-white  opacity-50 hover:opacity-100 hover:cursor-pointer max-w-96 hover:border-orange-300">
                     <p class="p-2.5 text-justify">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Neque molestiae corporis maiores ullam
-                        iste praesentium officia vel. Sed quaerat sunt iste magnam architecto, suscipit velit libero
-                        deserunt natus maxime maiores.</p></div>
+                        iste praesentium officia vel. 
+                        <br> <br>
+                        <div class="pi pi-phone" style="font-size: 15px; vertical-align:middle;"></div>  Telephone: +255 123-456-780 
+                        <br>
+                        <div class="pi pi-inbox" style="font-size: 15px; vertical-align:middle;"></div>  Email Addr: test@example.com
+                        <br>
+                        <div class="pi pi-link" style="font-size: 15px; vertical-align:middle;"></div>   Website:   <a href="https://www.google.com" @click="showAlert" target="_blank">www.students-school.aca</a> 
+                        <br>
+                        <div class="pi pi-twitter" style="font-size: 15px; vertical-align:middle;"></div>  Formerly twitter | <div class="pi pi-facebook" style="font-size: 15px; vertical-align:middle;"></div> Facebook
+                    </p></div>
                 <div
                     class="rounded border border-white text-white  opacity-50 hover:opacity-100 hover:cursor-pointer max-w-96 hover:border-orange-300">
                     <p class="p-2.5 text-justify">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Neque molestiae corporis maiores ullam
-                        iste praesentium officia vel. Sed quaerat sunt iste magnam architecto, suscipit velit libero
-                        deserunt natus maxime maiores.</p></div>
+                        iste praesentium officia vel. 
+                        <br> <br>
+                        <div class="pi pi-phone" style="font-size: 15px; vertical-align:middle;"></div>  Telephone: +255 123-456-780 
+                        <br>
+                        <div class="pi pi-inbox" style="font-size: 15px; vertical-align:middle;"></div>  Email Addr: test@example.com
+                        <br>
+                        <div class="pi pi-link" style="font-size: 15px; vertical-align:middle;"></div>   Website:   <a href="https://www.google.com" @click="showAlert" target="_blank">www.students-school.aca</a> 
+                        <br>
+                        <div class="pi pi-twitter" style="font-size: 15px; vertical-align:middle;"></div>  Formerly twitter | <div class="pi pi-facebook" style="font-size: 15px; vertical-align:middle;"></div> Facebook
+                    </p></div>
                 <div
                     class="rounded border border-white text-white  opacity-50 hover:opacity-100 hover:cursor-pointer max-w-96 hover:border-orange-300">
                     <p class="p-2.5 text-justify">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Neque molestiae corporis maiores ullam
-                        iste praesentium officia vel. Sed quaerat sunt iste magnam architecto, suscipit velit libero
-                        deserunt natus maxime maiores.</p></div>
+                        iste praesentium officia vel. 
+                        <br> <br>
+                        <div class="pi pi-phone" style="font-size: 15px; vertical-align:middle;"></div>  Telephone: +255 123-456-780 
+                        <br>
+                        <div class="pi pi-inbox" style="font-size: 15px; vertical-align:middle;"></div>  Email Addr: test@example.com
+                        <br>
+                        <div class="pi pi-link" style="font-size: 15px; vertical-align:middle;"></div>   Website:   <a href="https://www.google.com" @click="showAlert" target="_blank">www.students-school.aca</a> 
+                        <br>
+                        <div class="pi pi-twitter" style="font-size: 15px; vertical-align:middle;"></div>  Formerly twitter | <div class="pi pi-facebook" style="font-size: 15px; vertical-align:middle;"></div> Facebook
+                    </p></div>
                 <div
                     class="rounded border border-white text-white  opacity-50 hover:opacity-100 hover:cursor-pointer max-w-96 hover:border-orange-300">
                     <p class="p-2.5 text-justify">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Neque molestiae corporis maiores ullam
-                        iste praesentium officia vel. Sed quaerat sunt iste magnam architecto, suscipit velit libero
-                        deserunt natus maxime maiores.</p></div>
+                        iste praesentium officia vel. 
+                        <br> <br>
+                        <div class="pi pi-phone" style="font-size: 15px; vertical-align:middle;"></div>  Telephone: +255 123-456-780 
+                        <br>
+                        <div class="pi pi-inbox" style="font-size: 15px; vertical-align:middle;"></div>  Email Addr: test@example.com
+                        <br>
+                        <div class="pi pi-link" style="font-size: 15px; vertical-align:middle;"></div>   Website:   <a href="https://www.google.com" @click="showAlert" target="_blank">www.students-school.aca</a> 
+                        <br>
+                        <div class="pi pi-twitter" style="font-size: 15px; vertical-align:middle;"></div>  Formerly twitter | <div class="pi pi-facebook" style="font-size: 15px; vertical-align:middle;"></div> Facebook
+                    </p></div>
                 <div
                     class="rounded border border-white text-white  opacity-50 hover:opacity-100 hover:cursor-pointer max-w-96 hover:border-orange-300">
                     <p class="p-2.5 text-justify">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Neque molestiae corporis maiores ullam
-                        iste praesentium officia vel. Sed quaerat sunt iste magnam architecto, suscipit velit libero
-                        deserunt natus maxime maiores.</p></div>
+                        iste praesentium officia vel. 
+                        <br> <br>
+                        <div class="pi pi-phone" style="font-size: 15px; vertical-align:middle;"></div>  Telephone: +255 123-456-780 
+                        <br>
+                        <div class="pi pi-inbox" style="font-size: 15px; vertical-align:middle;"></div>  Email Addr: test@example.com
+                        <br>
+                        <div class="pi pi-link" style="font-size: 15px; vertical-align:middle;"></div>   Website:   <a href="https://www.google.com" @click="showAlert" target="_blank">www.students-school.aca</a> 
+                        <br>
+                        <div class="pi pi-twitter" style="font-size: 15px; vertical-align:middle;"></div>  Formerly twitter | <div class="pi pi-facebook" style="font-size: 15px; vertical-align:middle;"></div> Facebook
+                    </p></div>
                 <div
                     class="rounded border border-white text-white  opacity-50 hover:opacity-100 hover:cursor-pointer max-w-96 hover:border-orange-300">
                     <p class="p-2.5 text-justify">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Neque molestiae corporis maiores ullam
-                        iste praesentium officia vel. Sed quaerat sunt iste magnam architecto, suscipit velit libero
-                        deserunt natus maxime maiores.</p></div>
+                        iste praesentium officia vel. 
+                        <br> <br>
+                        <div class="pi pi-phone" style="font-size: 15px; vertical-align:middle;"></div>  Telephone: +255 123-456-780 
+                        <br>
+                        <div class="pi pi-inbox" style="font-size: 15px; vertical-align:middle;"></div>  Email Addr: test@example.com
+                        <br>
+                        <div class="pi pi-link" style="font-size: 15px; vertical-align:middle;"></div>   Website:   <a href="https://www.google.com" @click="showAlert" target="_blank">www.students-school.aca</a> 
+                        <br>
+                        <div class="pi pi-twitter" style="font-size: 15px; vertical-align:middle;"></div>  Formerly twitter | <div class="pi pi-facebook" style="font-size: 15px; vertical-align:middle;"></div> Facebook
+                    </p></div>
                
 
             </div>
