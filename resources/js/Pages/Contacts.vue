5d776fc06f1a8f30b75302f29f107bd624d18ba9
jmutua <johnstone.m10@gmail.com>
2024-11-25 12:55:15 +0300

    grids added

--- resources/js/Pages/Contacts.vue
+++ resources/js/Pages/Contacts.vue
@@ -3,7 +3,12 @@
     <AuthenticatedLayout>
         <div class="min-h-screen bg-purple-50 bg-gradient-to-r from-purple-950 via-purple-800 to-purple-950 ... ">
             <h1 class="text-white text-8xl text-center pt-28 font-bold">Our Locations</h1>
-
+            <div class="grid grid-rows-4 grid-flow-col gap-4">
+                <div>01</div>
+                <div>02</div>
+                <div>03</div>
+                <div>04</div>
+            </div>
 
 
 
