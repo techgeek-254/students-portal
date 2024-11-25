130a938bfc36aa82f118e4ce01d87e6dea1c7939
jmutua <johnstone.m10@gmail.com>
2024-11-25 15:39:43 +0300

    primeicons added

--- resources/js/Pages/Contacts.vue
+++ resources/js/Pages/Contacts.vue
@@ -2,11 +2,12 @@
 
     <AuthenticatedLayout>
         <div class="p-20 bg-purple-50 bg-gradient-to-r from-purple-950 via-purple-950 to-purple-950 ... ">
-            <h1 class="text-white text-8xl text-center pt-15 font-bold pb-10">Our Locations</h1>
+            <h1 class="text-white text-8xl text-center pt-15 font-bold pb-10">Our Locations <div class="pi pi-map-marker" style="font-size: 20px; vertical-align: top;"></div></h1>
             <div class="grid grid-rows-4 grid-flow-col gap-4">
                 <div
                     class="rounded border border-white text-white  opacity-50 hover:opacity-100 hover:cursor-grab max-w-96 hover:border-orange-300">
-                    <img src="" alt="">
+                    <div></div>
+                    
                     <p class="p-2.5 text-justify">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Neque molestiae corporis maiores ullam
                         iste praesentium officia vel. Sed quaerat sunt iste magnam architecto, suscipit velit libero
                         deserunt natus maxime maiores.</p>
@@ -81,5 +82,7 @@
 
 <script setup>
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
+
+import 'primeicons/primeicons.css'
 
 </script>
