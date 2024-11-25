367e0d172769d0fef13db7409880348a9c6d1272
jmutua <johnstone.m10@gmail.com>
2024-11-25 12:02:36 +0300

    Created contacts page and added inertia router in web.php

---
+++ resources/js/Pages/Contacts.vue
@@ -0,0 +1,18 @@
+<template>
+
+    <AuthenticatedLayout>
+        <Link href="contact">Contacts</Link>
+
+        <div>
+    <h1>Testing Page</h1>
+</div>
+    </AuthenticatedLayout>
+
+</template>
+
+
+<script setup>
+import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
+import { Link } from '@inertiajs/vue3';
+
+</script>
--- routes/web.php
+++ routes/web.php
@@ -26,6 +26,7 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 
     Route::resource('students', StudentController::class);
+    Route::inertia('contacts', 'Contacts');
 });
 
 require __DIR__.'/auth.php';
