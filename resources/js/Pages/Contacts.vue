517b427b4c07c92c60748f9036423f26fc6c69e0
jmutua <johnstone.m10@gmail.com>
2024-11-25 14:58:37 +0300

    styling

--- resources/js/Pages/Contacts.vue
+++ resources/js/Pages/Contacts.vue
@@ -1,21 +1,71 @@
 <template>
 
     <AuthenticatedLayout>
-        <div class="min-h-screen bg-purple-50 bg-gradient-to-r from-purple-950 via-purple-800 to-purple-950 ... ">
-            <h1 class="text-white text-8xl text-center pt-28 font-bold">Our Locations</h1>
+        <div class="p-20 bg-purple-50 bg-gradient-to-r from-purple-950 via-purple-950 to-purple-950 ... ">
+            <h1 class="text-white text-8xl text-center pt-15 font-bold pb-10">Our Locations</h1>
             <div class="grid grid-rows-4 grid-flow-col gap-4">
-                <div>01</div>
-                <div>02</div>
-                <div>03</div>
-                <div>04</div>
-                <div>01</div>
-                <div>02</div>
-                <div>03</div>
-                <div>04</div>
-                <div>01</div>
-                <div>02</div>
-                <div>03</div>
-                <div>04</div>
+                <div
+                    class="rounded border border-white text-white  opacity-50 hover:opacity-100 hover:cursor-grab max-w-96 hover:border-orange-300">
+                    <p class="p-2.5 text-justify">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Neque molestiae corporis maiores ullam
+                        iste praesentium officia vel. Sed quaerat sunt iste magnam architecto, suscipit velit libero
+                        deserunt natus maxime maiores.</p>
+                </div>
+                <div
+                    class="rounded border border-white text-white  opacity-50 hover:opacity-100 hover:cursor-pointer max-w-96 hover:border-orange-300">
+                    <p class="p-2.5 text-justify">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Neque molestiae corporis maiores ullam
+                        iste praesentium officia vel. Sed quaerat sunt iste magnam architecto, suscipit velit libero
+                        deserunt natus maxime maiores.</p></div>
+                <div
+                    class="rounded border border-white text-white  opacity-50 hover:opacity-100 hover:cursor-pointer max-w-96 hover:border-orange-300">
+                    <p class="p-2.5 text-justify">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Neque molestiae corporis maiores ullam
+                        iste praesentium officia vel. Sed quaerat sunt iste magnam architecto, suscipit velit libero
+                        deserunt natus maxime maiores.</p></div>
+                <div
+                    class="rounded border border-white text-white  opacity-50 hover:opacity-100 hover:cursor-pointer max-w-96 hover:border-orange-300">
+                    <p class="p-2.5 text-justify">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Neque molestiae corporis maiores ullam
+                        iste praesentium officia vel. Sed quaerat sunt iste magnam architecto, suscipit velit libero
+                        deserunt natus maxime maiores.</p></div>
+                <div
+                    class="rounded border border-white text-white  opacity-50 hover:opacity-100 hover:cursor-pointer max-w-96 hover:border-orange-300">
+                    <p class="p-2.5 text-justify">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Neque molestiae corporis maiores ullam
+                        iste praesentium officia vel. Sed quaerat sunt iste magnam architecto, suscipit velit libero
+                        deserunt natus maxime maiores.</p></div>
+                <div
+                    class="rounded border border-white text-white  opacity-50 hover:opacity-100 hover:cursor-pointer max-w-96 hover:border-orange-300">
+                    <p class="p-2.5 text-justify">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Neque molestiae corporis maiores ullam
+                        iste praesentium officia vel. Sed quaerat sunt iste magnam architecto, suscipit velit libero
+                        deserunt natus maxime maiores.</p></div>
+                <div
+                    class="rounded border border-white text-white  opacity-50 hover:opacity-100 hover:cursor-pointer max-w-96 hover:border-orange-300">
+                    <p class="p-2.5 text-justify">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Neque molestiae corporis maiores ullam
+                        iste praesentium officia vel. Sed quaerat sunt iste magnam architecto, suscipit velit libero
+                        deserunt natus maxime maiores.</p></div>
+                <div
+                    class="rounded border border-white text-white  opacity-50 hover:opacity-100 hover:cursor-pointer max-w-96 hover:border-orange-300">
+                    <p class="p-2.5 text-justify">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Neque molestiae corporis maiores ullam
+                        iste praesentium officia vel. Sed quaerat sunt iste magnam architecto, suscipit velit libero
+                        deserunt natus maxime maiores.</p></div>
+                <div
+                    class="rounded border border-white text-white  opacity-50 hover:opacity-100 hover:cursor-pointer max-w-96 hover:border-orange-300">
+                    <p class="p-2.5 text-justify">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Neque molestiae corporis maiores ullam
+                        iste praesentium officia vel. Sed quaerat sunt iste magnam architecto, suscipit velit libero
+                        deserunt natus maxime maiores.</p></div>
+                <div
+                    class="rounded border border-white text-white  opacity-50 hover:opacity-100 hover:cursor-pointer max-w-96 hover:border-orange-300">
+                    <p class="p-2.5 text-justify">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Neque molestiae corporis maiores ullam
+                        iste praesentium officia vel. Sed quaerat sunt iste magnam architecto, suscipit velit libero
+                        deserunt natus maxime maiores.</p></div>
+                <div
+                    class="rounded border border-white text-white  opacity-50 hover:opacity-100 hover:cursor-pointer max-w-96 hover:border-orange-300">
+                    <p class="p-2.5 text-justify">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Neque molestiae corporis maiores ullam
+                        iste praesentium officia vel. Sed quaerat sunt iste magnam architecto, suscipit velit libero
+                        deserunt natus maxime maiores.</p></div>
+                <div
+                    class="rounded border border-white text-white  opacity-50 hover:opacity-100 hover:cursor-pointer max-w-96 hover:border-orange-300">
+                    <p class="p-2.5 text-justify">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Neque molestiae corporis maiores ullam
+                        iste praesentium officia vel. Sed quaerat sunt iste magnam architecto, suscipit velit libero
+                        deserunt natus maxime maiores.</p></div>
+               
 
             </div>
 
