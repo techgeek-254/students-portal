4e5604972d59dc7b30d89b7b5bbd6139d2624eb4
jmutua <johnstone.m10@gmail.com>
2024-11-26 15:52:42 +0300

    styling

--- resources/js/Layouts/GuestLayout.vue
+++ resources/js/Layouts/GuestLayout.vue
@@ -4,23 +4,15 @@ import { Link } from '@inertiajs/vue3';
 </script>
 
 <template>
-    <div
-        class="flex min-h-screen flex-col items-center bg-purple-100 pt-6 sm:justify-center sm:pt-0"
-    >
+    <div class="flex min-h-screen flex-col items-center pt-6 sm:justify-center sm:pt-0 bg-purple-100">
         <div>
-            <a href="/">
-                <img src="/resources/img/logo.svg" alt="loginPage_logo">
-            </a>
-
 
             <Link href="/">
-                <ApplicationLogo class="h-20 w-20 fill-current text-gray-500" />
+            <ApplicationLogo class="h-20 w-20 fill-current text-gray-500" />
             </Link>
         </div>
 
-        <div
-            class="mt-6 w-full overflow-hidden bg-white px-6 py-4 shadow-md sm:max-w-md sm:rounded-lg"
-        >
+        <div class="mt-6 w-full overflow-hidden bg-white px-6 py-4 shadow-md sm:max-w-md sm:rounded-lg">
             <slot />
         </div>
     </div>
