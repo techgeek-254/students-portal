617d1a49cb2d6f4dffb1843a19d45a667299913d
jmutua <johnstone.m10@gmail.com>
2024-11-26 08:31:21 +0300

    title change in the app.vue file

--- resources/js/app.js
+++ resources/js/app.js
@@ -12,7 +12,7 @@ import 'vue-toastification/dist/index.css';
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
 
 createInertiaApp({
-    title: (title) => `${title} - ${appName}`,
+    title: (title) => `${title}`,
     resolve: (name) =>
         resolvePageComponent(
             `./Pages/${name}.vue`,
