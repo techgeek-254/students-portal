4e9e432bb69d29adf9272243e39eb647a8678e92
jmutua <johnstone.m10@gmail.com>
2024-12-03 16:22:58 +0300

    styling 404 page

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -8,22 +8,23 @@
     ```
   -->
  <!DOCTYPE html>
- <html lang="en">
+ <html lang="en" class="h-full">
  <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>404 - Page Not Found</title>
     <script src="https://cdn.tailwindcss.com"></script>
+    
  </head>
- <body>
- <main class="grid min-h-full place-items-center bg-white px-6 py-24 sm:py-32 lg:px-8">
-    <div class="text-center">
-      <p class="text-base font-semibold text-purple-600">404</p>
+ <body class="h-full">
+ <main class="grid min-h-full place-items-center bg-white px-6 py-24 sm:py-32 lg:px-8 ">
+    <div class="text-center border border-gray-100 px-20 py-20 rounded-lg shadow-md hover:ring-purple-200 hover:ring">
+      <p class=" font-semibold text-purple-800 text-7xl">404</p>
       <h1 class="mt-4 text-balance text-5xl font-semibold tracking-tight text-gray-900 sm:text-7xl">Page not found</h1>
       <p class="mt-6 text-pretty text-lg font-medium text-gray-500 sm:text-xl/8">Sorry, we couldn’t find the page you’re looking for.</p>
       <div class="mt-10 flex items-center justify-center gap-x-6">
-        <a href="#" class="rounded-md bg-purple-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Go back home</a>
-        <a href="#" class="text-sm font-semibold text-gray-900">Contact support <span aria-hidden="true">&rarr;</span></a>
+        <a href="dashboard" class="rounded-md bg-purple-800 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-purple-600 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-purple-600">Go back home</a>
+        <a href="contacts" class="text-sm font-semibold text-gray-900">Contact IT Support <span aria-hidden="true">&rarr;</span></a>
       </div>
     </div>
   </main>
